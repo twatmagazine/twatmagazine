d8cbcbce5ab4e0ec0d1d7e016928de29e70bfd23
root <root@ip-172-31-43-166.us-west-2.compute.internal>
2014-05-13 21:08:04 +0000

    First commit!

---
+++ Twat/MagBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Twat\MagBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TwatMagBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
