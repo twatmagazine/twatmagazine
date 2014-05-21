52d48fbe97ed681627168b5468393c2397eb9941
twatmagazine <twatmagazine@outlook.com>
2014-05-21 22:14:45 +0100

    ContactForm

--- src/Twat/MagBundle/Controller/PageController.php
+++ src/Twat/MagBundle/Controller/PageController.php
@@ -3,6 +3,8 @@
 namespace Twat\MagBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Twat\MagBundle\Entity\Enquiry;
+use Twat\MagBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -15,4 +17,23 @@ class PageController extends Controller
     {
         return $this->render('TwatMagBundle:Page:about.html.twig');
     }
+
+	public function contactAction()
+	{
+	    $enquiry = new Enquiry();
+	    $form = $this->createForm(new EnquiryType(), $enquiry);
+
+	    $request = $this->getRequest();
+	    if ($request->getMethod() == 'POST') {
+	        $form->bindRequest($request);
+
+	        if ($form->isValid()) {
+	            return $this->redirect($this->generateUrl('TwatMagBundle_contact'));
+	        }
+	    }
+
+	    return $this->render('TwatMagBundle:Page:contact.html.twig', array(
+	        'form' => $form->createView()
+	    ));
+	}
 }
