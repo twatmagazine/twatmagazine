f9c9ffa9d8c0f23e37f83dd4678e5536c4fea4b1
twatmagazine <twatmagazine@outlook.com>
2014-05-25 16:32:57 +0100

    blogcontroller2

--- src/Twat/MagBundle/Controller/BlogController.php
+++ src/Twat/MagBundle/Controller/BlogController.php
@@ -29,10 +29,10 @@ class BlogController extends Controller
             throw $this->createNotFoundException('Unable to find Blog post.');
         }
 
-        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+        $comments = $em->getRepository('TwatMagBundle:Comment')
                        ->getCommentsForBlog($blog->getId());
 
-        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+        return $this->render('TwatMagBundle:Blog:show.html.twig', array(
             'blog'      => $blog,
             'comments'  => $comments
         ));
