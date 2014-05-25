025c52d378ef528117fb8417b1977951429c3fe2
twatmagazine <twatmagazine@outlook.com>
2014-05-25 13:53:22 +0100

    timestamp

--- src/Twat/MagBundle/Entity/Blog.php
+++ src/Twat/MagBundle/Entity/Blog.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="blog")
+ * @ORM\HasLifecycleCallbacks
  */
 class Blog
 {
@@ -223,5 +224,19 @@ class Blog
     public function getUpdated()
     {
         return $this->updated;
+    }
+
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedValue()
+    {
+       $this->setUpdated(new \DateTime());
     }
 }
