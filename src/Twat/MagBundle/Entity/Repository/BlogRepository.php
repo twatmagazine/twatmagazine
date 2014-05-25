f595804b9c6af79649920f21ac4c14efdebb4087
twatmagazine <twatmagazine@outlook.com>
2014-05-25 20:57:08 +0100

    sidebar5

--- src/Twat/MagBundle/Entity/Repository/BlogRepository.php
+++ src/Twat/MagBundle/Entity/Repository/BlogRepository.php
@@ -26,50 +26,50 @@ class BlogRepository extends EntityRepository
     }
 
     public function getTags()
-{
-    $blogTags = $this->createQueryBuilder('b')
-                     ->select('b.tags')
-                     ->getQuery()
-                     ->getResult();
-
-    $tags = array();
-    foreach ($blogTags as $blogTag)
     {
-        $tags = array_merge(explode(",", $blogTag['tags']), $tags);
+        $blogTags = $this->createQueryBuilder('b')
+                         ->select('b.tags')
+                         ->getQuery()
+                         ->getResult();
+
+        $tags = array();
+        foreach ($blogTags as $blogTag)
+        {
+            $tags = array_merge(explode(",", $blogTag['tags']), $tags);
+        }
+
+        foreach ($tags as &$tag)
+        {
+            $tag = trim($tag);
+        }
+
+        return $tags;
     }
 
-    foreach ($tags as &$tag)
+    public function getTagWeights($tags)
     {
-        $tag = trim($tag);
-    }
+        $tagWeights = array();
+        if (empty($tags))
+            return $tagWeights;
 
-    return $tags;
-}
+        foreach ($tags as $tag)
+        {
+            $tagWeights[$tag] = (isset($tagWeights[$tag])) ? $tagWeights[$tag] + 1 : 1;
+        }
+        // Shuffle the tags
+        uksort($tagWeights, function() {
+            return rand() > rand();
+        });
 
-public function getTagWeights($tags)
-{
-    $tagWeights = array();
-    if (empty($tags))
+        $max = max($tagWeights);
+
+        // Max of 5 weights
+        $multiplier = ($max > 5) ? 5 / $max : 1;
+        foreach ($tagWeights as &$tag)
+        {
+            $tag = ceil($tag * $multiplier);
+        }
+
         return $tagWeights;
-
-    foreach ($tags as $tag)
-    {
-        $tagWeights[$tag] = (isset($tagWeights[$tag])) ? $tagWeights[$tag] + 1 : 1;
     }
-    // Shuffle the tags
-    uksort($tagWeights, function() {
-        return rand() > rand();
-    });
-
-    $max = max($tagWeights);
-
-    // Max of 5 weights
-    $multiplier = ($max > 5) ? 5 / $max : 1;
-    foreach ($tagWeights as &$tag)
-    {
-        $tag = ceil($tag * $multiplier);
-    }
-
-    return $tagWeights;
-}
 }
